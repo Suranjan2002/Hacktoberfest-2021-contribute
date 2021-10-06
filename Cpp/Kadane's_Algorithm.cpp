cf804749827b4bf348cd3af1c3e84ed59c46fa3b
Manjur Alam Middey <83777365+manjur2904@users.noreply.github.com>
2021-10-06 16:37:27 +0530

    Added code for Kadane's Algorithm

---
+++ Cpp/Kadane's_Algorithm.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        int mxSum = INT_MIN;
+        int sum = 0;
+        for(int i=0; i<n; i++)
+        {
+            sum += arr[i];
+            mxSum = max(mxSum, sum);
+            if(sum < 0)
+                sum = 0;
+        }
+        return mxSum;
+        
+    }
+};
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}

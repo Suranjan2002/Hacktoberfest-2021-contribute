6a567321c5a0d2235c7b1d9a76d2d795ff7eca45
Kuladeep <kuladeep.nitw@gmail.com>
2021-10-07 12:35:48 +0530

    added no of testcases condition

--- Python/Armstrong Number.py
+++ Python/Armstrong Number.py
@@ -1,19 +1,23 @@
 # take input from the user
-num = input("Enter a number: ")
-dig=len(num)
-nums=list(map(int,num))
+N=int(input())
+while N!=0:
+   num = input("Enter a number: ")
+   dig=len(num)
+   nums=list(map(int,num))
 
-# initialize empty list sum1 
-sum1 = []
-for i in range(len(nums)):
-   t= nums[i] ** dig
-   sum1.append(t)
+   # initialize empty list sum1
+   sum1 = []
+   for i in range(len(nums)):
+      t= nums[i] ** dig
+      sum1.append(t)
 
-# display the result
-if int(num) == sum(sum1):
-   print(num,"is an Armstrong number")
-else:
-   print(num,"is not an Armstrong number")
+   # display the result
+   if int(num) == sum(sum1):
+      print(num,"is an Armstrong number")
+   else:
+      print(num,"is not an Armstrong number")
+
+   N=N-1
 
 
 

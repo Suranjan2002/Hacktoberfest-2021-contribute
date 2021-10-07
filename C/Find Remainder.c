8b1eac92891995a7a8d174c9ef259dd224174bc8
Avijit Ram <89923957+Avijit2002@users.noreply.github.com>
2021-10-07 10:31:42 +0530

    Find Remainder.c

---
+++ C/Find Remainder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+  {
+    int A, B, R;
+    printf("Enter A: ");
+    scanf("%d", &A);
+    printf("Enter B: ");
+    scanf("%d", &B);
+     R=A%B;
+    printf("Remainder = %d", R);
+}
+    

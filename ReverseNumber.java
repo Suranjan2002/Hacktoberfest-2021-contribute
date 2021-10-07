138b7d0cd48d9b4b914bd29c3e9b9c1ea9d9c6d5
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2021-10-07 19:00:53 +0530

    ReverseNumber.java
    please check it and accept my PRs.

---
+++ ReverseNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class ReverseNumber
+{
+	static int reverse(int n) 
+	{
+		int r,s=0;
+		while(n>0)
+		{
+			r=n%10;
+			s=s*10+r;
+			n=n/10;
+		}
+		return s;
+	}
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int T= sc.nextInt();
+		for (int i= 0;i< T;i++) 
+		{
+      System.out.println("Enter a number to reverse it ");
+			int n= sc.nextInt();
+			int k=reverse(n);
+			System.out.println(k);
+		}
+	}
+}

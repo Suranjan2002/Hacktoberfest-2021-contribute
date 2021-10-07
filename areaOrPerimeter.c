d51b44a2e07153f2505d2b960f4076052a1e922e
sourav das <91898457+sourav153@users.noreply.github.com>
2021-10-07 10:49:42 +0530

    area or perimeter

---
+++ areaOrPerimeter.c
@@ -0,0 +1,25 @@
+/* C Program To Calculate Perimeter & Area Of Rectangle*/
+#include<stdio.h>
+int main()
+{
+	int length, breadth, perimeter, area;
+
+	printf("Enter length and breadth: ");
+	scanf("%d%d", &length, &breadth);
+	
+	area = length*breadth;
+	perimeter = 2 * (length + breadth);
+	
+	printf("Area is : %d",area);
+	printf("\nPerimeter is : %d",perimeter);
+	
+	if (area>perimeter)
+		printf("\nArea is greater than perimeter");
+	else if (area<perimeter)
+		printf("Area is lesser than perimeter");
+	else
+		printf("Area and perimeter are equal");
+
+	return 0;
+}
+
